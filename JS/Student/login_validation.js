4acfd2cf5d15e230068cbef08e977fcf35555d33
skopos <lochanalaparthy@gmail.com>
2020-09-18 19:13:37 +0530

    Fixed login validation
    Should remove comments in js files only by backspacing or deleting can't use ctrl+?/

--- JS/Student/login_validation.js
+++ JS/Student/login_validation.js
@@ -1,16 +1,16 @@
 function LoginValidation(){
     document.write("hello");
-    // var em = document.getElementById("email");
-    // var pas = document.getElementById("pass");
-    // if (/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/.test(em)){
-    //     if(length(pas) == 8){
-    //         window.location.href = "https://anjali-rgpt.github.io/PlacementPortal/HTML/Student/companies_list.html#";
-    //     }
-    //     else{
-    //         alert("You have entered an invalid password!");
-    //     }
-    // }
-    // else{
-    //     alert("You have entered an invalid email address!");
-    // }
+    var em = document.getElementById("email");
+    var pas = document.getElementById("pass");
+    if (/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/.test(em)){
+        if(length(pas) == 8){
+            window.location.href = "https://anjali-rgpt.github.io/PlacementPortal/HTML/Student/companies_list.html#";
+        }
+        else{
+            alert("You have entered an invalid password!");
+        }
+    }
+    else{
+        alert("You have entered an invalid email address!");
+    }
 };
