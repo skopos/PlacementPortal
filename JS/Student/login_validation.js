0b66432480151e69503baa4f540f0e914f307b1e
anjali-rgpt <anjur99@gmail.com>
2020-09-18 18:42:29 +0530

    Added return true and false in login validation comments

--- JS/Student/login_validation.js
+++ JS/Student/login_validation.js
@@ -5,12 +5,15 @@ function LoginValidation(){
     // if (/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/.test(em)){
     //     if(length(pas) == 8){
     //         window.location.href = "https://anjali-rgpt.github.io/PlacementPortal/HTML/Student/companies_list.html#";
+    //         return true;
     //     }
     //     else{
     //         alert("You have entered an invalid password!");
+    //         return false;
     //     }
     // }
     // else{
     //     alert("You have entered an invalid email address!");
+    //     return false;
     // }
 }
