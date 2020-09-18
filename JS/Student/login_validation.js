83abad7e3b9d87653b17736f226f98767b0820e2
skopos <lochanalaparthy@gmail.com>
2020-09-19 00:38:24 +0530

    Finished Email validation

--- JS/Student/login_validation.js
+++ JS/Student/login_validation.js
@@ -1,20 +1,39 @@
-const emailRegex=/^[a-z][a-z\d]+@(gmail|hotmail|yahoo)\.com$/
+function Symbolcheck(str){
+    const val_regex = /[!|#|$|%|^|&|*|(|)|+|-|*|/|+|{|}|;|:|'|"|<|>|,|~|`|\s]/;
+
+    return (!val_regex.test(str));
+}
+
+function Splitcheck(str){
+    var split1 = str.split("@");
+    var split2 = str.split(".");
+    return (split1.length == 2 && split2.length == 2 && !(/[.]/.test(split1[0])) && !(/[@]/.test(split2[1])));
+}
+
+function Endcheck(str){
+    const val_regex = /[0-9_@.]+/
+
+    var split1 = str.split("@");
+    var split2 = split1[1].split(".");
+    return (!val_regex.test(split2[1]) && !val_regex.test(split2[0]));
+}
 
 function LoginValidation(){
     var em = document.getElementById("email").value;
     var pas = document.getElementById("pass").value;
-    document.write(em);
-    // if(!emailRegex.test(em)){
-    //     if(length(pas) == 8){
-    //         window.location.href = "https://anjali-rgpt.github.io/PlacementPortal/HTML/Student/companies_list.html#";
-    //     }
-    //     else{
-    //         alert("You have entered an invalid password!");
-    //         document.write(em);
-    //     }
-    // }
-    // else{
-    //     alert("You have entered an invalid email address!");
-    //     document.write(em);
-    // }
+
+    if(Symbolcheck(em) && Splitcheck(em) && Endcheck(em)){
+
+        if((String(pas)).length >= 8){
+            window.location.href = "https://anjali-rgpt.github.io/PlacementPortal/HTML/Student/mainpage.html#";
+        }
+
+        else{
+            alert("You have entered an invalid password!");
+        }
+    }
+
+    else{
+        alert("You have entered an invalid email address!");
+    }
 };
