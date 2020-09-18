6c3273fae5ffe8d4ec2cc0a70eef4c47c453c090
anjali-rgpt <anjur99@gmail.com>
2020-09-18 18:17:08 +0530

    fixed rollnumber regex

--- JS/Student/registrationvalidation.js
+++ JS/Student/registrationvalidation.js
@@ -2,8 +2,8 @@ function validate(event){
 
     const stringRegex=/^[A-Z][a-z]+$/
     const phoneNumRegex=/^[789]\d{9}$/
-    const rollNumRegex=/^(CB\.EN)(U4|P2)(CSE|ECE|MEE|EEE|CIV|CHE)[1][78][0123456][0123456][\d{1}]$/
-    const emailRegex=/^[a-z][a-z\d]+[@](gmail|hotmail|yahoo)[.]com$/
+    const rollNumRegex=/^CB\.EN\.(U4|P2)(CSE|ECE|MEE|EEE|CIV|CHE)[1][78][0123456][0123456][\d{1}]$/
+    const emailRegex=/^[a-z][a-z\d]+@(gmail|hotmail|yahoo)\.com$/
     
     var flag=0;
 
@@ -25,29 +25,29 @@ function validate(event){
     if (!isValid){
         alert('Name is not valid string expression.');
         console.log(fname, mname, lname);
-        event.preventDefault();
+        return false;
 
     }
 
-    var rollNum=document.getElementById('rollnum').value;
+    var rollNum=document.getElementById('rollnumber').value;
     if(!rollNumRegex.test(rollNum)){
-        alert('Invalid Roll Number format entered');
         console.log(rollNum);
-        event.preventDefault();
+        alert('Invalid Roll Number format entered');
+        return false;
     }
 
     var emailiD=document.getElementById('email').value;
     if(!emailRegex.test(emailiD)){
         console.log(emailiD);
         alert('Invalid email format');
-        event.preventDefault();
+        return false;
     }
 
     var phone=document.getElementById('phonenum').value;
     if(!phoneNumRegex.test(toString(phone))){
-        console.log(phone);
+        console.log(toString(phone));
         alert('Invalid phone number');
-        event.preventDefault();
+        return false;
     }
 
     var cgpa=document.getElementById('cgpa').value;
@@ -55,30 +55,26 @@ function validate(event){
     if (temp.contains('e')){
         console.log(temp);
         alert('Invalid number entered');
-        event.preventDefault();
+        return false;
     }
 
     else if(parseFloat(temp)>10.0 || parseFloat(temp)<0.0){
         console.log(temp)
         alert('Bad range');
-        event.preventDefault();
+        return false;
     }
 
     company=document.getElementById('companybox').value;
     if(company.length==0){
         alert('No company selected');
-        event.preventDefault();
+        return false;
     }
 
-    while (flag){
-        event.preventDefault();
-        console.log('Here');
-    }
-
+  return true;
     }
 
     catch(err){
         console.log(err);
-        event.preventDefault();
+        return false;
     }
 }
